0faa9cf4b5b13975465926442f22a4be30e4996c
Raymond Wijaya Gautama <raymondwijaya05@gmail.com>
2026-03-30 17:59:05 +0700

    menyambungkan ke mysql

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/test-db', function () {
+    try {
+        // Test koneksi
+        DB::connection()->getPdo();
+        
+        $siswa = DB::table('mst_siswa')->get();
+        
+        return $siswa;
+        
+    } catch (\Exception $e) {
+        return "Gagal nyambung ke database nih. Error: " . $e->getMessage();
+    }
 });
