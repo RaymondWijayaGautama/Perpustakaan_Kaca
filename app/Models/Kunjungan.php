075fac2f2700351bf7c0688d9aaa4f66136f421e
dylan0407 <155320689+dylan0407@users.noreply.github.com>
2026-04-09 23:14:29 +0700

    Update Kunjungan.php

--- app/Models/Kunjungan.php
+++ app/Models/Kunjungan.php
@@ -2,9 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model; // Ini wajib ada agar fungsi database bisa dipakai
+use Illuminate\Database\Eloquent\Model;
 
-class Kunjungan extends Model // Tulisan "extends Model" ini adalah kuncinya
+class Kunjungan extends Model
 {
     protected $table = 'tr_kunjungan_perpus'; 
     protected $primaryKey = 'id_kunjungan_perpus'; 
