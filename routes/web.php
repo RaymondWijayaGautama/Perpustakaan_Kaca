981482b3e3d212e4a737a366a8ca608572456340
Raymond Wijaya Gautama <raymondwijaya05@gmail.com>
2026-04-10 15:23:09 +0700

    benerin routes dikit supaya generate barcode

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\KoleksiController;
+
+Route::get('/generate-barcode', [KoleksiController::class, 'index']);
+Route::post('/generate-barcode', [KoleksiController::class, 'generate']);
 
 Route::get('/', function () {
     return view('welcome');
